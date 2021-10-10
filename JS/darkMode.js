1a73a66bcd45ea36676a9bbc6e8c2996d45727f6
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-10 19:40:08 +0300

    now the search bar also changes bg color

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,21 +1,28 @@
 function darkModeSave() {
 	// This code is only used to remember theme selection between page loads
-	const themeSwitch = document.querySelector('.theme-switch');
-	themeSwitch.checked = localStorage.getItem('switchedTheme') === 'true';
+	const themeSwitch = document.querySelector(".theme-switch");
+	themeSwitch.checked = localStorage.getItem("switchedTheme") === "true";
 	document.body.style.backgroundColor = localStorage.getItem("bgColor");
 	document.body.style.color = localStorage.getItem("textColor");
-	themeSwitch.addEventListener('change', function(e) {
+	document.getElementById("myInput").style.backgroundColor = localStorage.getItem("myInputBgColor");
+	document.getElementById("myInput").style.color = localStorage.getItem("myInputTextColor");
+	
+	themeSwitch.addEventListener("change", function(e) {
 		if (e.currentTarget.checked === true) {
 			// Add item to localstorage
-			localStorage.setItem('switchedTheme', 'true');
+			localStorage.setItem("switchedTheme", "true");
 			localStorage.setItem("bgColor","#404040");
 			localStorage.setItem("textColor","white");
+			localStorage.setItem("myInputBgColor","#404040");
+			localStorage.setItem("myInputTextColor","white");
 			location.reload();
 		} else {
 			// Remove item if theme is switched back to normal
-			localStorage.removeItem('switchedTheme');
-			localStorage.setItem("bgColor","white");
-			localStorage.setItem("textColor","black");
+			localStorage.removeItem("switchedTheme");
+			localStorage.removeItem("bgColor");
+			localStorage.removeItem("textColor");
+			localStorage.removeItem("myInputBgColor");
+			localStorage.removeItem("myInputTextColor");
 			location.reload();
 		}
 	});
