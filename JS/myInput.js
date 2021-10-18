7fedd1d98a15a07ff2d62f72c3fcf8fe7e05f5a8
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-18 13:36:19 +0300

    fixed character button for better search results

--- JS/myInput.js
+++ JS/myInput.js
@@ -5,6 +5,41 @@ function searchTable () {
 			$("#sortTable tr").filter(function() {
 				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 			});
+			
+			if($( "#btnAttack" ).hasClass("active")){
+				$(".trAssist").hide();
+				$(".trDefense").hide();
+				$(".trSkill").hide();
+				$(".trUtility").hide();
+			}
+			
+			if($( "#btnAssist" ).hasClass("active")){
+				$(".trAttack").hide();
+				$(".trDefense").hide();
+				$(".trSkill").hide();
+				$(".trUtility").hide();
+			}
+			
+			if($( "#btnDefense" ).hasClass("active")){
+				$(".trAttack").hide();
+				$(".trAssist").hide();
+				$(".trSkill").hide();
+				$(".trUtility").hide();
+			}
+			
+			if($( "#btnSkill" ).hasClass("active")){
+				$(".trAttack").hide();
+				$(".trAssist").hide();
+				$(".trDefense").hide();
+				$(".trUtility").hide();
+			}
+			
+			if($( "#btnUtility" ).hasClass("active")){
+				$(".trAttack").hide();
+				$(".trAssist").hide();
+				$(".trSkill").hide();
+				$(".trDefense").hide();
+			}
 		});
 	});
 }
