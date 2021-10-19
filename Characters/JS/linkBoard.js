43c03874f96d6cef12e91568971abd95279b10ed
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-19 17:38:19 +0300

    created a basic table for the link board

---
+++ Characters/JS/linkBoard.js
@@ -0,0 +1,6 @@
+function linkBoard(){
+	let i;
+	for(i = 70; i < 71; i ++){
+		document.getElementsByClassName("abilityPoints").item(i).src = "https://lh3.googleusercontent.com/pkaGH5QJyR0OerBecH4eYv3ZR4AeAdVThepkfPMMCzvIPk3GhwV9Ey9BUCf4Ca2lgI_bWGw82SNzayzNArjzAUkytFFgKyAl-e0ogPgsgl__9JtweOYhr_9ACqsVKF3pkLf9uxnUjQ=w1920-h1080";
+	}
+}
