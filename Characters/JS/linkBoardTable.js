bd665701b0e1bf384b74addb9cb118691f9f4e2b
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-20 20:15:41 +0300

    completed the path 1 of the linkBoard

--- Characters/JS/linkBoardTable.js
+++ Characters/JS/linkBoardTable.js
@@ -1,13 +1,13 @@
 function linkBoardTable(){
 	let linkBoardTableRows = [[]] , i;
 	
-	for(i = 1; i < 61; i ++){
+	for(i = 1; i < 81; i ++){
 		linkBoardTableRows[i] = document.getElementById("linkBoardTable").rows[i].cells;
 		linkBoardTableRows[i][0].innerHTML = i;
 	}
 	
-	for(i = 1; i < 61; i ++){
-		if(i === 21 || i === 40){
+	for(i = 1; i < 81; i ++){
+		if(i === 21 || i === 40 || i === 61){
 			linkBoardTableRows[i][3].innerHTML = "80";
 			linkBoardTableRows[i][5].innerHTML = "40";
 			linkBoardTableRows[i][7].innerHTML = "10";
@@ -33,6 +33,17 @@ function linkBoardTable(){
 			linkBoardTableRows[i][5].innerHTML = "10";
 			linkBoardTableRows[i][7].innerHTML = "1000000";
 			linkBoardTableRows[i][9].innerHTML = "20000";
+		}else if(i === 79){
+			linkBoardTableRows[i][3].innerHTML = "80";
+			linkBoardTableRows[i][5].innerHTML = "40";
+			linkBoardTableRows[i][7].innerHTML = "10";
+			linkBoardTableRows[i][11].innerHTML = "1000000";
+			linkBoardTableRows[i][13].innerHTML = "1000000";
+			linkBoardTableRows[i][15].innerHTML = "10";
+		}else if(i === 80){
+			linkBoardTableRows[i][5].innerHTML = "1000000";
+			linkBoardTableRows[i][7].innerHTML = "1000000";
+			linkBoardTableRows[i][9].innerHTML = "10";
 		}else{
 			linkBoardTableRows[i][5].innerHTML = "1000000";
 			linkBoardTableRows[i][7].innerHTML = "1000000";
@@ -40,20 +51,20 @@ function linkBoardTable(){
 		}
 	}
 	
-	for(i = 0; i < 53; i ++){
-		document.getElementsByClassName("increaseCharStat").item(i).src = "https://lh3.googleusercontent.com/A5z6_L451I2qgM3XClqvKx4F-abkTDybL0qW6UMaqx09LdIjr8iPr9C2vpNTZvga2eoOQwq37Dgo7llIwn72XKAxZacb-HRljfhkpnagEJBiC_pc8wSHYpiTJmFx9lTHB7b2CKyJUA=w1920-h1080";
-	}
-	
-	for(i = 0; i < 53; i ++){
-		document.getElementsByClassName("linkBoardRyo").item(i).src = "https://lh3.googleusercontent.com/BJl_cwfBPySnObV0BjX_w2xx32LBuqcKzSwefpm9SqEDc_--d90_iyI31mX-JGKswtwtTLYRNsvcaoNCKSCeSygu1iO5k5p9mngqQItMA30B0ic6Kn1TN0tedsk_pg2630dgFmKnEQ=w1920-h1080";
-	}
-	
-	for(i = 0; i < 58; i ++){
+	for(i = 0; i < 77; i ++){
 		document.getElementsByClassName("linkBoardChakra").item(i).src = "https://lh3.googleusercontent.com/G-BAMQLBMFxzA09MgJmD7KF650eMIbGDnr5DURaAXUGEDRy6tvPhEhNGD4_ufZG-QlmgIJCFmQ3L0gFJ9ZpQ3QRrLaOK_F-SPMFC4GXQj5PC4CtO113S3418MGRYhEEJMxbcWNN62w=w1920-h1080";
 	}
 	
-	for(i = 0; i < 58; i ++){
+	for(i = 0; i < 75; i ++){
 		document.getElementsByClassName("linkBoardAbilityPoints").item(i).src = "https://lh3.googleusercontent.com/pkaGH5QJyR0OerBecH4eYv3ZR4AeAdVThepkfPMMCzvIPk3GhwV9Ey9BUCf4Ca2lgI_bWGw82SNzayzNArjzAUkytFFgKyAl-e0ogPgsgl__9JtweOYhr_9ACqsVKF3pkLf9uxnUjQ=w1920-h1080";
+	}
+	
+	for(i = 0; i < 72; i ++){
+		document.getElementsByClassName("linkBoardRyo").item(i).src = "https://lh3.googleusercontent.com/BJl_cwfBPySnObV0BjX_w2xx32LBuqcKzSwefpm9SqEDc_--d90_iyI31mX-JGKswtwtTLYRNsvcaoNCKSCeSygu1iO5k5p9mngqQItMA30B0ic6Kn1TN0tedsk_pg2630dgFmKnEQ=w1920-h1080";
+	}
+	
+	for(i = 0; i < 70; i ++){
+		document.getElementsByClassName("increaseCharStat").item(i).src = "https://lh3.googleusercontent.com/A5z6_L451I2qgM3XClqvKx4F-abkTDybL0qW6UMaqx09LdIjr8iPr9C2vpNTZvga2eoOQwq37Dgo7llIwn72XKAxZacb-HRljfhkpnagEJBiC_pc8wSHYpiTJmFx9lTHB7b2CKyJUA=w1920-h1080";
 	}
 	
 	for(i = 0; i < 2; i ++){
@@ -64,21 +75,29 @@ function linkBoardTable(){
 		document.getElementsByClassName("reduceResistancePoison").item(i).src = "https://lh3.googleusercontent.com/sdJkV2XHcZw7wtYfVgza89obyUnNKEn94tFNXGJaYZQ4y7n6YpymCioWvWSRg2jFUVptyY8pMEpXMarhRGT2Zerw2hVdkzRxHTglmrduOG90OWcaHHqueE0i0dv-f86wV01zP68fXQ=w1920-h1080";
 	}
 	
-	for(i = 0; i < 3; i ++){
-		document.getElementsByClassName("shinobiAmulet").item(i).src = "https://lh3.googleusercontent.com/AsyE9VSFIoLCSlEAkbulQExe4ZOupW7gqLa0AmLVb-Mxd9vD2CQzJWqOQ1Zb_1Itvy0ZAtuyBbiNfoIBMZGioWFAK3zlf1AVaovcGPlCxPuVQ5L4uDM1EgPoDKJJ4JEsHplaE0THVQ=w1920-h1080";
+	for(i = 0; i < 2; i ++){
+		document.getElementsByClassName("increaseCharCardsTools").item(i).src = "https://lh3.googleusercontent.com/n5GUOcvrzEyOsZcsEdN1Sr9_F8c5CgrTHRjRxnG05c426eCeMWUVQ4lghQo6AyPTRfYupr3N3MDgyqdrQwJhm6qJm2_vyw-PGl__akKkHBIndA0ZDjjGQbeffrRw1YLAQDkq8rrS4A=w1920-h1080";
 	}
 	
 	for(i = 0; i < 1; i ++){
 		document.getElementsByClassName("linkBoard5StarAttackGem").item(i).src = "https://lh3.googleusercontent.com/mF65xMho503dM5aqaW1Uk8Zo8MAsejJSiUZKc78ggwFAi6ooMofMem0W9W9oeSEsn7PGeiDMJ2idQr65aXdCCJu7HlHoHNTnZVpB-tN5LDfZPEKFmimARRG4nwSXtPy67y8daPGaeA=w1920-h1080";
 	}
 	
-	for(i = 0; i < 4; i ++){
+	for(i = 0; i < 3; i ++){
+		document.getElementsByClassName("shinobiAmulet").item(i).src = "https://lh3.googleusercontent.com/AsyE9VSFIoLCSlEAkbulQExe4ZOupW7gqLa0AmLVb-Mxd9vD2CQzJWqOQ1Zb_1Itvy0ZAtuyBbiNfoIBMZGioWFAK3zlf1AVaovcGPlCxPuVQ5L4uDM1EgPoDKJJ4JEsHplaE0THVQ=w1920-h1080";
+	}
+	
+	for(i = 0; i < 2; i ++){
+		document.getElementsByClassName("expansionScroll").item(i).src = "https://lh3.googleusercontent.com/b1sLIN6FyUYSa_EMvZMOrKdltQ4D6am5VnE_wDf-hstkQeKonUH9q1oa3flT7evLbE0-94jp2SAsS6Ul5iZwguPZHkqZA0e8h4l2WXhACiH2EUZuGLmaVn2OE62h4wdHleYT9iIkuw=w1920-h1080";
+	}
+	
+	for(i = 0; i < 6; i ++){
 		document.getElementsByClassName("linkBoardLocked").item(i).src = "https://lh3.googleusercontent.com/c4ixXBI6aKwDBekfHUqSL6HjVPFd3L7-NI9Nf29qCKFW50Di-Dq7nRVjUd-5D8egBDPeuQGIoPL3pCMMM0mmlb70sIZ5UMIlfrUtmxVGmzFf_jLTu0xJUCisKSVYn9PCvvfS07vKOw=w1920-h1080";
 		document.getElementsByClassName("linkBoardSuperFragment").item(i).src = "https://lh3.googleusercontent.com/aEdNb7OiL9xoARn_h-t7E1hwa9li2OE07i_S6J3RbhnfbCqgkgfSWOq_yDRE9492qLMhsudtBbzkTLn1se3Hi53Vv6sX2pss2XigzT92u1zZqN1Wil_MatKsDAwGUVBYjMopAII4eA=w1920-h1080";
 		document.getElementsByClassName("linkBoardKeys").item(i).src = "https://lh3.googleusercontent.com/7s5B9I501ay5wHi_04Oc-5qz2YIiNiSElMCF0Xyo5imnf06RXKBwaiQMdsuVwo9mBJ5ImlGE08pa5JFnai3D8d7-9jgQCOKve1mH8LTgXr3kV1ynbN4WKEuQ5MT6USoCna9DPLpTcg=w1920-h1080";
 	}
 	
-	for(i = 0; i < 5; i ++){
+	for(i = 0; i < 7; i ++){
 		document.getElementsByClassName("linkBoardFragment").item(i).src = "https://lh3.googleusercontent.com/snf81ECMK7Zn1_RpDH-DtMy6DvagAyrAJCT7Q9p9hlV-_wl4yrEqm1AwXaK5oEZWDStyMh88qy4TbCwFf4ska7C4oi_td0uUtmW4DeHc8_RehkPjIF60wfS-RucIJHVDtX7FYzlSwA=w1920-h1080";
 	}
 	
@@ -151,4 +170,28 @@ function linkBoardTable(){
 	linkBoardTableRows[58][4].innerHTML = "+600 HP";
 	linkBoardTableRows[59][4].innerHTML = "+900 ATK";
 	linkBoardTableRows[60][4].innerHTML = "+10% Ultimate Jutsu Resistance";
+	
+	linkBoardTableRows[62][4].innerHTML = "+400 ATK";
+	linkBoardTableRows[63][4].innerHTML = "+200 HP";
+	linkBoardTableRows[64][4].innerHTML = "+600 HP";
+	linkBoardTableRows[65][4].innerHTML = "+800 HP";
+	
+	linkBoardTableRows[66][4].innerHTML = "+400 ATK";
+	linkBoardTableRows[67][4].innerHTML = "+40 CP";
+	linkBoardTableRows[68][4].innerHTML = "+80 CP";
+	linkBoardTableRows[69][4].innerHTML = "+160 CP";
+	
+	linkBoardTableRows[70][4].innerHTML = "+800 ATK";
+	linkBoardTableRows[71][4].innerHTML = "+120 DEF";
+	linkBoardTableRows[72][4].innerHTML = "+240 DEF";
+	linkBoardTableRows[73][4].innerHTML = "+360 DEF";
+	
+	linkBoardTableRows[74][4].innerHTML = "+1000 ATK";
+	linkBoardTableRows[75][4].innerHTML = "+400 HP";
+	linkBoardTableRows[76][4].innerHTML = "+120 CP";
+	linkBoardTableRows[77][4].innerHTML = "+480 DEF";
+	
+	linkBoardTableRows[78][4].innerHTML = "+1200 ATK";
+	linkBoardTableRows[79][10].innerHTML = "+1 Ninja Card Equipment Slot(s)";
+	linkBoardTableRows[80][4].innerHTML = "+1 Ninja Tool Equipment Slot(s)";
 }
