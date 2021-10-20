53cce5854e45cb771caf1d91ff22559d10c9f5a1
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-20 13:12:58 +0300

    added the first 30 of the linkBoard

--- Characters/JS/abilityListTable.js
+++ Characters/JS/abilityListTable.js
@@ -1,16 +1,16 @@
 function abilityListTable(){
-	let tableRows = [[]] , i;
-	tableRows[0] = document.getElementById("abilityListTable").rows[0].cells;
-	tableRows[1] = document.getElementById("abilityListTable").rows[1].cells;
+	let abilityListTableRows = [[]] , i;
+	abilityListTableRows[0] = document.getElementById("abilityListTable").rows[0].cells;
+	abilityListTableRows[1] = document.getElementById("abilityListTable").rows[1].cells;
 	
-	switch(tableRows[0][2].innerHTML){
+	switch(abilityListTableRows[1][1].innerHTML){
 		case "69":
-			for(i = 2; i < 72; i ++){
-				tableRows[i] = document.getElementById("abilityListTable").rows[i].cells;
+			for(i = 3; i < 80; i ++){
+				abilityListTableRows[i] = document.getElementById("abilityListTable").rows[i].cells;
 			}
 			
-			for(i = 2; i < 71; i ++){
-				tableRows[i][0].innerHTML = i - 1;
+			for(i = 3; i < 72; i ++){
+				abilityListTableRows[i][0].innerHTML = i - 2;
 			}
 			
 			for(i = 0; i < 70; i ++){
@@ -25,229 +25,240 @@ function abilityListTable(){
 				document.getElementsByClassName("increaseUltimatePower").item(i).src = "https://lh3.googleusercontent.com/cBHO4JCxyfpAKvYvPAAWcNL7jVI_Kz7ijBLo_jMYTG_mrrlzxlO8eW6zQvwbGfrlrK--ucfPnTkkKklFfMpi19kAVBAj2mEZeT1Ofd0KCFs-jbuQISkSgPjJsn_QkyKQ3rw4NaX8nA=w1920-h1080";
 			}
 			
-			tableRows[2][1].innerHTML = "1";
-			tableRows[2][2].innerHTML = "+10 HP";
-			tableRows[2][4].innerHTML = 30;
+			abilityListTableRows[3][1].innerHTML = "1";
+			abilityListTableRows[3][2].innerHTML = "+10 HP";
+			abilityListTableRows[3][4].innerHTML = 30;
 			
-			tableRows[3][2].innerHTML = "+10 ATK";
-			tableRows[3][4].innerHTML = 30;
+			abilityListTableRows[4][2].innerHTML = "+10 ATK";
+			abilityListTableRows[4][4].innerHTML = 30;
 			
-			tableRows[4][2].innerHTML = "+15 DEF";
-			tableRows[4][4].innerHTML = 60;
+			abilityListTableRows[5][2].innerHTML = "+15 DEF";
+			abilityListTableRows[5][4].innerHTML = 60;
 			
-			tableRows[5][2].innerHTML = "+10 CP";
-			tableRows[5][4].innerHTML = 60;
+			abilityListTableRows[6][2].innerHTML = "+10 CP";
+			abilityListTableRows[6][4].innerHTML = 60;
 			
-			tableRows[6][2].innerHTML = "+10 ATK/+5 CP";
-			tableRows[6][4].innerHTML = 60;
+			abilityListTableRows[7][2].innerHTML = "+10 ATK/+5 CP";
+			abilityListTableRows[7][4].innerHTML = 60;
 			
-			tableRows[7][2].innerHTML = "+20 HP/+20 ATK";
-			tableRows[7][4].innerHTML = 400;
+			abilityListTableRows[8][2].innerHTML = "+20 HP/+20 ATK";
+			abilityListTableRows[8][4].innerHTML = 400;
 			
-			tableRows[8][2].innerHTML = "+30 ATK";
-			tableRows[8][4].innerHTML = 400;
+			abilityListTableRows[9][2].innerHTML = "+30 ATK";
+			abilityListTableRows[9][4].innerHTML = 400;
 			
-			tableRows[9][2].innerHTML = "+30 HP/+50 DEF";
-			tableRows[9][4].innerHTML = 1200;
+			abilityListTableRows[10][2].innerHTML = "+30 HP/+50 DEF";
+			abilityListTableRows[10][4].innerHTML = 1200;
 			
-			tableRows[10][1].innerHTML = "2";
-			tableRows[10][2].innerHTML = "+20 CP";
-			tableRows[10][4].innerHTML = 400;
+			abilityListTableRows[11][1].innerHTML = "2";
+			abilityListTableRows[11][2].innerHTML = "+20 CP";
+			abilityListTableRows[11][4].innerHTML = 400;
 			
-			tableRows[11][2].innerHTML = "+5 CP/+30 ATK";
-			tableRows[11][4].innerHTML = 400;
+			abilityListTableRows[12][2].innerHTML = "+5 CP/+30 ATK";
+			abilityListTableRows[12][4].innerHTML = 400;
 			
-			tableRows[12][2].innerHTML = "+10 HP/+10 ATK/+15 DEF/+5 CP";
-			tableRows[12][4].innerHTML = 400;
+			abilityListTableRows[13][2].innerHTML = "+10 HP/+10 ATK/+15 DEF/+5 CP";
+			abilityListTableRows[13][4].innerHTML = 400;
 			
-			tableRows[13][2].innerHTML = "+40 HP/+30 ATK";
-			tableRows[13][4].innerHTML = 1200;
+			abilityListTableRows[14][2].innerHTML = "+40 HP/+30 ATK";
+			abilityListTableRows[14][4].innerHTML = 1200;
 			
-			tableRows[14][2].innerHTML = "+50 ATK/+15 DEF";
-			tableRows[14][4].innerHTML = 1200;
+			abilityListTableRows[15][2].innerHTML = "+50 ATK/+15 DEF";
+			abilityListTableRows[15][4].innerHTML = 1200;
 			
-			tableRows[15][2].innerHTML = "+50 HP/+1% ATK";
-			tableRows[15][4].innerHTML = 1600;
+			abilityListTableRows[16][2].innerHTML = "+50 HP/+1% ATK";
+			abilityListTableRows[16][4].innerHTML = 1600;
 			
-			tableRows[16][2].innerHTML = "+80 ATK/+12% CRI";
-			tableRows[16][4].innerHTML = 2000;
+			abilityListTableRows[17][2].innerHTML = "+80 ATK/+12% CRI";
+			abilityListTableRows[17][4].innerHTML = 2000;
 			
-			tableRows[17][1].innerHTML = "3";
-			tableRows[17][2].innerHTML = "+80 ATK/+10 CP";
-			tableRows[17][4].innerHTML = 1600;
+			abilityListTableRows[18][1].innerHTML = "3";
+			abilityListTableRows[18][2].innerHTML = "+80 ATK/+10 CP";
+			abilityListTableRows[18][4].innerHTML = 1600;
 			
-			tableRows[18][2].innerHTML = "+15% CRI/+10% EVA";
-			tableRows[18][4].innerHTML = 2000;
+			abilityListTableRows[19][2].innerHTML = "+15% CRI/+10% EVA";
+			abilityListTableRows[19][4].innerHTML = 2000;
 			
-			tableRows[19][2].innerHTML = "+100 ATK/+100 HP";
-			tableRows[19][4].innerHTML = 4000;
+			abilityListTableRows[20][2].innerHTML = "+100 HP/+100 ATK";
+			abilityListTableRows[20][4].innerHTML = 4000;
 			
-			tableRows[20][2].innerHTML = "+500 ATK/+15 CP/+10% CRI";
-			tableRows[20][4].innerHTML = 5000;
+			abilityListTableRows[21][2].innerHTML = "+500 ATK/+15 CP/+10% CRI";
+			abilityListTableRows[21][4].innerHTML = 5000;
 			
-			tableRows[21][2].innerHTML = "POW +50/+40 HP/+2% ATK";
-			tableRows[21][5].innerHTML = 4000;
+			abilityListTableRows[22][2].innerHTML = "POW +50/+40 HP/+2% ATK";
+			abilityListTableRows[22][5].innerHTML = 4000;
 			
-			tableRows[22][1].innerHTML = "4";
-			tableRows[22][2].innerHTML = "+50 HP/+50 ATK";
-			tableRows[22][4].innerHTML = 2000;
+			abilityListTableRows[23][1].innerHTML = "4";
+			abilityListTableRows[23][2].innerHTML = "+50 HP/+50 ATK";
+			abilityListTableRows[23][4].innerHTML = 2000;
 			
-			tableRows[23][2].innerHTML = "+5% ATK";
-			tableRows[23][4].innerHTML = 4000;
+			abilityListTableRows[24][2].innerHTML = "+5% ATK";
+			abilityListTableRows[24][4].innerHTML = 4000;
 			
-			tableRows[24][2].innerHTML = "+80 HP/+80 ATK/+90 DEF";
-			tableRows[24][4].innerHTML = 5000;
+			abilityListTableRows[25][2].innerHTML = "+80 HP/+80 ATK/+90 DEF";
+			abilityListTableRows[25][4].innerHTML = 5000;
 			
-			tableRows[25][2].innerHTML = "+3% ATK/+70 DEF/+20% CRI/+12% EVA";
-			tableRows[25][4].innerHTML = 7000;
+			abilityListTableRows[26][2].innerHTML = "+3% ATK/+70 DEF/+20% CRI/+12% EVA";
+			abilityListTableRows[26][4].innerHTML = 7000;
 			
-			tableRows[26][2].innerHTML = "CP -4/+100 ATK/+90 DEF/+15 CP";
-			tableRows[26][5].innerHTML = 4000;
+			abilityListTableRows[27][2].innerHTML = "CP -4/+100 ATK/+90 DEF/+15 CP";
+			abilityListTableRows[27][5].innerHTML = 4000;
 			
-			tableRows[27][2].innerHTML = "CD -0.7/+80 HP/+200 ATK";
-			tableRows[27][5].innerHTML = 4000;
+			abilityListTableRows[28][2].innerHTML = "CD -0.7/+80 HP/+200 ATK";
+			abilityListTableRows[28][5].innerHTML = 4000;
 			
-			tableRows[28][2].innerHTML = "POW +60/CP -8/CD -1";
-			tableRows[28][5].innerHTML = 4000;
+			abilityListTableRows[29][2].innerHTML = "POW +60/CP -8/CD -1";
+			abilityListTableRows[29][5].innerHTML = 4000;
 			
-			tableRows[29][1].innerHTML = "5";
-			tableRows[29][2].innerHTML = "+100 ATK/+90 DEF/+80 CP";
-			tableRows[29][4].innerHTML = 5000;
+			abilityListTableRows[30][1].innerHTML = "5";
+			abilityListTableRows[30][2].innerHTML = "+100 ATK/+90 DEF/+80 CP";
+			abilityListTableRows[30][4].innerHTML = 5000;
 			
-			tableRows[30][2].innerHTML = "+500 HP/+500 ATK";
-			tableRows[30][4].innerHTML = 7000;
+			abilityListTableRows[31][2].innerHTML = "+500 HP/+500 ATK";
+			abilityListTableRows[31][4].innerHTML = 7000;
 			
-			tableRows[31][2].innerHTML = "+4% HP/+300 ATK";
-			tableRows[31][4].innerHTML = 8000;
+			abilityListTableRows[32][2].innerHTML = "+4% HP/+300 ATK";
+			abilityListTableRows[32][4].innerHTML = 8000;
 			
-			tableRows[32][2].innerHTML = "+9% ATK";
-			tableRows[32][4].innerHTML = 10000;
+			abilityListTableRows[33][2].innerHTML = "+9% ATK";
+			abilityListTableRows[33][4].innerHTML = 10000;
 			
-			tableRows[33][2].innerHTML = "POW +3%/+50 HP/+200 ATK";
-			tableRows[33][5].innerHTML = 7000;
+			abilityListTableRows[34][2].innerHTML = "POW +3%/+50 HP/+200 ATK";
+			abilityListTableRows[34][5].innerHTML = 7000;
 			
-			tableRows[34][2].innerHTML = "CP -3%/+100 HP/+90 DEF";
-			tableRows[34][5].innerHTML = 7000;
+			abilityListTableRows[35][2].innerHTML = "CP -3%/+100 HP/+90 DEF";
+			abilityListTableRows[35][5].innerHTML = 7000;
 			
-			tableRows[35][2].innerHTML = "CD -1.2/+100 ATK/+90 DEF/+30 CP";
-			tableRows[35][5].innerHTML = 7000;
+			abilityListTableRows[36][2].innerHTML = "CD -1.2/+100 ATK/+90 DEF/+30 CP";
+			abilityListTableRows[36][5].innerHTML = 7000;
 			
-			tableRows[36][2].innerHTML = "POW +80/CP -12/+100 ATK";
-			tableRows[36][5].innerHTML = 7000;
+			abilityListTableRows[37][2].innerHTML = "POW +80/CP -12/+100 ATK";
+			abilityListTableRows[37][5].innerHTML = 7000;
 			
-			tableRows[37][2].innerHTML = "POW +90/CP -12/CD -1.2";
-			tableRows[37][5].innerHTML = 7000;
+			abilityListTableRows[38][2].innerHTML = "POW +90/CP -12/CD -1.2";
+			abilityListTableRows[38][5].innerHTML = 7000;
 			
-			tableRows[38][1].innerHTML = "6";
-			tableRows[38][2].innerHTML = "+300 HP/+100 ATK/+90 DEF/+30 CP";
-			tableRows[38][4].innerHTML = 8000;
+			abilityListTableRows[39][1].innerHTML = "6";
+			abilityListTableRows[39][2].innerHTML = "+300 HP/+100 ATK/+90 DEF/+30 CP";
+			abilityListTableRows[39][4].innerHTML = 8000;
 			
-			tableRows[39][2].innerHTML = "+100 HP/+1000 ATK/+200 DEF";
-			tableRows[39][4].innerHTML = 10000;
+			abilityListTableRows[40][2].innerHTML = "+100 HP/+1000 ATK/+200 DEF";
+			abilityListTableRows[40][4].innerHTML = 10000;
 			
-			tableRows[40][2].innerHTML = "+4% HP/+800 ATK/+70 DEF/+10 CP";
-			tableRows[40][4].innerHTML = 10000;
+			abilityListTableRows[41][2].innerHTML = "+4% HP/+800 ATK/+70 DEF/+10 CP";
+			abilityListTableRows[41][4].innerHTML = 10000;
 			
-			tableRows[41][2].innerHTML = "POW +6%";
-			tableRows[41][5].innerHTML = 10000;
+			abilityListTableRows[42][2].innerHTML = "POW +6%";
+			abilityListTableRows[42][5].innerHTML = 10000;
 			
-			tableRows[42][2].innerHTML = "CP -6%";
-			tableRows[42][5].innerHTML = 10000;
+			abilityListTableRows[43][2].innerHTML = "CP -6%";
+			abilityListTableRows[43][5].innerHTML = 10000;
 			
-			tableRows[43][2].innerHTML = "POW +70/CD -1.8/+100 HP/+200 ATK";
-			tableRows[43][5].innerHTML = 10000;
+			abilityListTableRows[44][2].innerHTML = "POW +70/CD -1.8/+100 HP/+200 ATK";
+			abilityListTableRows[44][5].innerHTML = 10000;
 			
-			tableRows[44][2].innerHTML = "POW +90/+6% ATK";
-			tableRows[44][5].innerHTML = 10000;
+			abilityListTableRows[45][2].innerHTML = "POW +90/+6% ATK";
+			abilityListTableRows[45][5].innerHTML = 10000;
 			
-			tableRows[45][2].innerHTML = "POW +50";
-			tableRows[45][5].innerHTML = 3000;
+			abilityListTableRows[46][2].innerHTML = "POW +50";
+			abilityListTableRows[46][5].innerHTML = 3000;
 			
-			tableRows[46][2].innerHTML = "CP -4";
-			tableRows[46][5].innerHTML = 2500;
+			abilityListTableRows[47][2].innerHTML = "CP -4";
+			abilityListTableRows[47][5].innerHTML = 2500;
 			
-			tableRows[47][2].innerHTML = "CD -0.7";
-			tableRows[47][5].innerHTML = 2500;
+			abilityListTableRows[48][2].innerHTML = "CD -0.7";
+			abilityListTableRows[48][5].innerHTML = 2500;
 			
-			tableRows[48][2].innerHTML = "POW +60/CP -8/CD -1";
-			tableRows[48][5].innerHTML = 4000;
+			abilityListTableRows[49][2].innerHTML = "POW +60/CP -8/CD -1";
+			abilityListTableRows[49][5].innerHTML = 4000;
 			
-			tableRows[49][2].innerHTML = "POW +3%";
-			tableRows[49][5].innerHTML = 5000;
+			abilityListTableRows[50][2].innerHTML = "POW +3%";
+			abilityListTableRows[50][5].innerHTML = 5000;
 			
-			tableRows[50][2].innerHTML = "CP -3%";
-			tableRows[50][5].innerHTML = 6000;
+			abilityListTableRows[51][2].innerHTML = "CP -3%";
+			abilityListTableRows[51][5].innerHTML = 6000;
 			
-			tableRows[51][2].innerHTML = "CD -1.2";
-			tableRows[51][5].innerHTML = 4000;
+			abilityListTableRows[52][2].innerHTML = "CD -1.2";
+			abilityListTableRows[52][5].innerHTML = 4000;
 			
-			tableRows[52][2].innerHTML = "POW +80/CP -12";
-			tableRows[52][5].innerHTML = 6000;
+			abilityListTableRows[53][2].innerHTML = "POW +80/CP -12";
+			abilityListTableRows[53][5].innerHTML = 6000;
 			
-			tableRows[53][2].innerHTML = "POW +90/CP -12/CD -1.2";
-			tableRows[53][5].innerHTML = 7000;
+			abilityListTableRows[54][2].innerHTML = "POW +90/CP -12/CD -1.2";
+			abilityListTableRows[54][5].innerHTML = 7000;
 			
-			tableRows[54][2].innerHTML = "POW +6%";
-			tableRows[54][5].innerHTML = 10000;
+			abilityListTableRows[55][2].innerHTML = "POW +6%";
+			abilityListTableRows[55][5].innerHTML = 10000;
 			
-			tableRows[55][2].innerHTML = "CP -6%";
-			tableRows[55][5].innerHTML = 10000;
+			abilityListTableRows[56][2].innerHTML = "CP -6%";
+			abilityListTableRows[56][5].innerHTML = 10000;
 			
-			tableRows[56][2].innerHTML = "POW +70/CD -1.8";
-			tableRows[56][5].innerHTML = 7000;
+			abilityListTableRows[57][2].innerHTML = "POW +70/CD -1.8";
+			abilityListTableRows[57][5].innerHTML = 7000;
 			
-			tableRows[57][2].innerHTML = "POW +90";
-			tableRows[57][5].innerHTML = 8500;
+			abilityListTableRows[58][2].innerHTML = "POW +90";
+			abilityListTableRows[58][5].innerHTML = 8500;
 			
-			tableRows[58][2].innerHTML = "POW +50";
-			tableRows[58][5].innerHTML = 3000;
+			abilityListTableRows[59][2].innerHTML = "POW +50";
+			abilityListTableRows[59][5].innerHTML = 3000;
 			
-			tableRows[59][2].innerHTML = "CP -4";
-			tableRows[59][5].innerHTML = 2500;
+			abilityListTableRows[60][2].innerHTML = "CP -4";
+			abilityListTableRows[60][5].innerHTML = 2500;
 			
-			tableRows[60][2].innerHTML = "CD -0.7";
-			tableRows[60][5].innerHTML = 2500;
+			abilityListTableRows[61][2].innerHTML = "CD -0.7";
+			abilityListTableRows[61][5].innerHTML = 2500;
 			
-			tableRows[61][2].innerHTML = "POW +60/CP -8/CD -1";
-			tableRows[61][5].innerHTML = 4000;
+			abilityListTableRows[62][2].innerHTML = "POW +60/CP -8/CD -1";
+			abilityListTableRows[62][5].innerHTML = 4000;
 			
-			tableRows[62][2].innerHTML = "POW +3%";
-			tableRows[62][5].innerHTML = 5000;
+			abilityListTableRows[63][2].innerHTML = "POW +3%";
+			abilityListTableRows[63][5].innerHTML = 5000;
 			
-			tableRows[63][2].innerHTML = "CP -3%";
-			tableRows[63][5].innerHTML = 6000;
+			abilityListTableRows[64][2].innerHTML = "CP -3%";
+			abilityListTableRows[64][5].innerHTML = 6000;
 			
-			tableRows[64][2].innerHTML = "CD -1.2";
-			tableRows[64][5].innerHTML = 4000;
+			abilityListTableRows[65][2].innerHTML = "CD -1.2";
+			abilityListTableRows[65][5].innerHTML = 4000;
 			
-			tableRows[65][2].innerHTML = "POW +80/CP -12";
-			tableRows[65][5].innerHTML = 6000;
+			abilityListTableRows[66][2].innerHTML = "POW +80/CP -12";
+			abilityListTableRows[66][5].innerHTML = 6000;
 			
-			tableRows[66][2].innerHTML = "POW +90/CP -12/CD -1.2";
-			tableRows[66][5].innerHTML = 7000;
+			abilityListTableRows[67][2].innerHTML = "POW +90/CP -12/CD -1.2";
+			abilityListTableRows[67][5].innerHTML = 7000;
 			
-			tableRows[67][2].innerHTML = "POW +6%";
-			tableRows[67][5].innerHTML = 10000;
+			abilityListTableRows[68][2].innerHTML = "POW +6%";
+			abilityListTableRows[68][5].innerHTML = 10000;
 			
-			tableRows[68][2].innerHTML = "CP -6%";
-			tableRows[68][5].innerHTML = 10000;
+			abilityListTableRows[69][2].innerHTML = "CP -6%";
+			abilityListTableRows[69][5].innerHTML = 10000;
 			
-			tableRows[69][2].innerHTML = "POW +70/CD -1.8";
-			tableRows[69][5].innerHTML = 7000;
+			abilityListTableRows[70][2].innerHTML = "POW +70/CD -1.8";
+			abilityListTableRows[70][5].innerHTML = 7000;
 			
-			tableRows[70][2].innerHTML = "POW +90";
-			tableRows[70][5].innerHTML = 8500;
+			abilityListTableRows[71][2].innerHTML = "POW +90";
+			abilityListTableRows[71][5].innerHTML = 8500;
 			
-			tableRows[71][1].innerHTML = 0;
+			abilityListTableRows[72][1].innerHTML = 0;
 			
-			for(i = 2; i < 71; i ++){
-				if(i < 21 || (i > 21 && i < 26) || (i > 28 && i < 33) || (i > 37 && i < 41)){
-					tableRows[71][1].innerHTML = eval("parseInt(tableRows[71][1].innerHTML) + parseInt(tableRows[i][4].innerHTML)");
+			for(i = 3; i < 72; i ++){
+				if(i < 22 || (i > 22 && i < 27) || (i > 29 && i < 34) || (i > 38 && i < 42)){
+					abilityListTableRows[72][1].innerHTML = eval("parseInt(abilityListTableRows[72][1].innerHTML) + parseInt(abilityListTableRows[i][4].innerHTML)");
 				}
-				if(i === 21 || (i > 25 && i < 29) || (i > 32 && i < 38) || (i > 40)){
-					tableRows[71][1].innerHTML = eval("parseInt(tableRows[71][1].innerHTML) + parseInt(tableRows[i][5].innerHTML)");
+				if(i === 22 || (i > 26 && i < 30) || (i > 33 && i < 39) || (i > 41)){
+					abilityListTableRows[72][1].innerHTML = eval("parseInt(abilityListTableRows[72][1].innerHTML) + parseInt(abilityListTableRows[i][5].innerHTML)");
 				}
 			}
+			
+			abilityListTableRows[74][0].innerHTML = "+1660";
+			abilityListTableRows[74][2].innerHTML = "+4780";
+			abilityListTableRows[74][4].innerHTML = "+975";
+			
+			abilityListTableRows[75][0].innerHTML = "+8.00%";
+			abilityListTableRows[75][2].innerHTML = "+26.00%";
+			abilityListTableRows[75][4].innerHTML = "+235";
+			
+			abilityListTableRows[76][0].innerHTML = "+57.00%";
+			abilityListTableRows[76][2].innerHTML = "+22.00%";
 			
 			break;
 	}
--- Characters/JS/linkBoardTable.js
+++ Characters/JS/linkBoardTable.js
@@ -1,6 +1,73 @@
 function linkBoardTable(){
-	let i;
-	for(i = 70; i < 71; i ++){
-		document.getElementsByClassName("abilityPoints").item(i).src = "https://lh3.googleusercontent.com/pkaGH5QJyR0OerBecH4eYv3ZR4AeAdVThepkfPMMCzvIPk3GhwV9Ey9BUCf4Ca2lgI_bWGw82SNzayzNArjzAUkytFFgKyAl-e0ogPgsgl__9JtweOYhr_9ACqsVKF3pkLf9uxnUjQ=w1920-h1080";
+	let linkBoardTableRows = [[]] , i;
+	
+	for(i = 1; i < 31; i ++){
+		linkBoardTableRows[i] = document.getElementById("linkBoardTable").rows[i].cells;
+		linkBoardTableRows[i][0].innerHTML = i;
 	}
+	
+	for(i = 0; i < 30; i ++){
+		document.getElementsByClassName("IncreaseCharStat").item(i).src = "https://lh3.googleusercontent.com/A5z6_L451I2qgM3XClqvKx4F-abkTDybL0qW6UMaqx09LdIjr8iPr9C2vpNTZvga2eoOQwq37Dgo7llIwn72XKAxZacb-HRljfhkpnagEJBiC_pc8wSHYpiTJmFx9lTHB7b2CKyJUA=w1920-h1080";
+		document.getElementsByClassName("linkBoardRyo").item(i).src = "https://lh3.googleusercontent.com/BJl_cwfBPySnObV0BjX_w2xx32LBuqcKzSwefpm9SqEDc_--d90_iyI31mX-JGKswtwtTLYRNsvcaoNCKSCeSygu1iO5k5p9mngqQItMA30B0ic6Kn1TN0tedsk_pg2630dgFmKnEQ=w1920-h1080";
+		document.getElementsByClassName("linkBoardChakra").item(i).src = "https://lh3.googleusercontent.com/G-BAMQLBMFxzA09MgJmD7KF650eMIbGDnr5DURaAXUGEDRy6tvPhEhNGD4_ufZG-QlmgIJCFmQ3L0gFJ9ZpQ3QRrLaOK_F-SPMFC4GXQj5PC4CtO113S3418MGRYhEEJMxbcWNN62w=w1920-h1080";
+		document.getElementsByClassName("linkBoardAbilityPoints").item(i).src = "https://lh3.googleusercontent.com/pkaGH5QJyR0OerBecH4eYv3ZR4AeAdVThepkfPMMCzvIPk3GhwV9Ey9BUCf4Ca2lgI_bWGw82SNzayzNArjzAUkytFFgKyAl-e0ogPgsgl__9JtweOYhr_9ACqsVKF3pkLf9uxnUjQ=w1920-h1080";
+	}
+	
+	for(i = 0; i < 1; i ++){
+		document.getElementsByClassName("linkBoardLocked").item(i).src = "https://lh3.googleusercontent.com/c4ixXBI6aKwDBekfHUqSL6HjVPFd3L7-NI9Nf29qCKFW50Di-Dq7nRVjUd-5D8egBDPeuQGIoPL3pCMMM0mmlb70sIZ5UMIlfrUtmxVGmzFf_jLTu0xJUCisKSVYn9PCvvfS07vKOw=w1920-h1080";
+		document.getElementsByClassName("linkBoardFragment").item(i).src = "https://lh3.googleusercontent.com/snf81ECMK7Zn1_RpDH-DtMy6DvagAyrAJCT7Q9p9hlV-_wl4yrEqm1AwXaK5oEZWDStyMh88qy4TbCwFf4ska7C4oi_td0uUtmW4DeHc8_RehkPjIF60wfS-RucIJHVDtX7FYzlSwA=w1920-h1080";
+		document.getElementsByClassName("linkBoardSuperFragment").item(i).src = "https://lh3.googleusercontent.com/aEdNb7OiL9xoARn_h-t7E1hwa9li2OE07i_S6J3RbhnfbCqgkgfSWOq_yDRE9492qLMhsudtBbzkTLn1se3Hi53Vv6sX2pss2XigzT92u1zZqN1Wil_MatKsDAwGUVBYjMopAII4eA=w1920-h1080";
+		document.getElementsByClassName("linkBoardKeys").item(i).src = "https://lh3.googleusercontent.com/7s5B9I501ay5wHi_04Oc-5qz2YIiNiSElMCF0Xyo5imnf06RXKBwaiQMdsuVwo9mBJ5ImlGE08pa5JFnai3D8d7-9jgQCOKve1mH8LTgXr3kV1ynbN4WKEuQ5MT6USoCna9DPLpTcg=w1920-h1080";
+	}
+	
+	for(i = 1; i < 31; i ++){
+		if(i<21 || i>21){
+			linkBoardTableRows[i][5].innerHTML = "1000000";
+			linkBoardTableRows[i][7].innerHTML = "1000000";
+			linkBoardTableRows[i][9].innerHTML = "5000";
+		}else{
+			linkBoardTableRows[i][3].innerHTML = "80";
+			linkBoardTableRows[i][5].innerHTML = "40";
+			linkBoardTableRows[i][7].innerHTML = "10";
+			linkBoardTableRows[i][11].innerHTML = "1000000";
+			linkBoardTableRows[i][13].innerHTML = "1000000";
+			linkBoardTableRows[i][15].innerHTML = "1000000";
+		}
+	}
+	
+	linkBoardTableRows[1][4].innerHTML = "+50 HP";
+	linkBoardTableRows[2][4].innerHTML = "+75 HP";
+	linkBoardTableRows[3][4].innerHTML = "+100 HP";
+	linkBoardTableRows[4][4].innerHTML = "+125 HP";
+	linkBoardTableRows[5][4].innerHTML = "+150 HP";
+	
+	linkBoardTableRows[6][4].innerHTML = "+10 CP";
+	linkBoardTableRows[7][4].innerHTML = "+15 CP";
+	linkBoardTableRows[8][4].innerHTML = "+20 CP";
+	linkBoardTableRows[9][4].innerHTML = "+25 CP";
+	linkBoardTableRows[10][4].innerHTML = "+30 CP";
+	
+	linkBoardTableRows[11][4].innerHTML = "+100 ATK";
+	linkBoardTableRows[12][4].innerHTML = "+150 ATK";
+	linkBoardTableRows[13][4].innerHTML = "+200 ATK";
+	linkBoardTableRows[14][4].innerHTML = "+250 ATK";
+	linkBoardTableRows[15][4].innerHTML = "+300 ATK";
+	
+	linkBoardTableRows[16][4].innerHTML = "+30 DEF";
+	linkBoardTableRows[17][4].innerHTML = "+45 DEF";
+	linkBoardTableRows[18][4].innerHTML = "+60 DEF";
+	linkBoardTableRows[19][4].innerHTML = "+75 DEF";
+	linkBoardTableRows[20][4].innerHTML = "+90 DEF";
+	
+	linkBoardTableRows[21][10].innerHTML = "+200 ATK";
+	linkBoardTableRows[22][4].innerHTML = "+300 ATK";
+	linkBoardTableRows[23][4].innerHTML = "+400 ATK";
+	linkBoardTableRows[24][4].innerHTML = "+500 ATK";
+	linkBoardTableRows[25][4].innerHTML = "+600 ATK";
+	
+	linkBoardTableRows[26][4].innerHTML = "+100 ATK";
+	linkBoardTableRows[27][4].innerHTML = "+40 HP";
+	linkBoardTableRows[28][4].innerHTML = "+60 DEF";
+	linkBoardTableRows[29][4].innerHTML = "+120 DEF";
+	linkBoardTableRows[30][4].innerHTML = "+200 HP";
 }
