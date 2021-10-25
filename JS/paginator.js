1b8a5e41145143d5a7341820d8e2fbf1af92ab9b
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-10-25 11:06:06 +0300

    made paginator more beautiful and added previous and next button for easier switch between pages

--- JS/paginator.js
+++ JS/paginator.js
@@ -1,25 +1,25 @@
 $(window).on('load' , function(){
-	let i;
-	let table , tr;
+	let i , table , tr;
 	const paginationDiv = document.getElementById("pagination");
 	table = document.getElementById("sortTable");
-	const paginatorLength = Math.ceil(table.rows.length / 10);
+	let tRowsLength = table.rows.length;
+	const paginatorLength = Math.ceil(tRowsLength / 10);//paginatorLength = roundup(105/10)= 11
 	tr = table.getElementsByTagName("tr");
 	
-	for(i = table.rows.length - 1; i > 10; i --){
+	for(i = tRowsLength - 1; i > 10; i --){
 		tr[i].style.display = "none";
 	}
 	
-	const aStart = document.createElement("a");
-	aStart.href = "#";
-	aStart.innerHTML = "&laquo;";
-	paginationDiv.appendChild(aStart);
+	const aPrevious = document.createElement("a");
+	aPrevious.href = "#";
+	aPrevious.innerHTML = "Previous";
+	paginationDiv.appendChild(aPrevious);
 	
 	for(i = 0; i < paginatorLength; i ++){
 		if(i === 0){
 			const a = document.createElement("a");
 			a.href = "#";
-			a.innerHTML = "1";
+			a.innerHTML = eval("i+1");
 			a.className = "active";
 			paginationDiv.appendChild(a);
 		}else{
@@ -30,44 +30,81 @@ $(window).on('load' , function(){
 		}
 	}
 	
-	const aEnd = document.createElement("a");
-	aEnd.href = "#";
-	aEnd.innerHTML = "&raquo;";
-	paginationDiv.appendChild(aEnd);
+	const aNext = document.createElement("a");
+	aNext.href = "#";
+	aNext.innerHTML = "Next";
+	paginationDiv.appendChild(aNext);
 	
 	let pagination = document.getElementById("pagination").children;
 	
-	for(let i = 0; i < paginatorLength+2; i ++){
+	pagination[0].addEventListener('click' , function(){
+		
+		for(let i = 2; i < paginatorLength + 1; i ++){
+			
+			if(pagination[i].className === "active"){
+				pagination[i - 1].className = "active";
+				pagination[i].className = "";
+				
+				for(let i = tRowsLength - 1; i > 0; i --){
+					tr[i].style.display = "none";
+				}
+				
+				if((i - 1) < paginatorLength + 1){
+					let sum = (((i - 1) - 1) * 10);
+					for(let i = sum + 1; i < sum + 11; i ++){
+						tr[i].style.display = "";
+					}
+				}
+			}
+		}
+		
+	});
+	
+	for(let i = 1; i < paginatorLength + 1; i ++){
 		pagination[i].addEventListener('click' , function(){
 			if(pagination[i].className !== "active"){
 				pagination[i].className = "active";
 			}
 			
-			for(let j = 0; j < paginatorLength+2; j ++){
+			for(let j = 1; j < paginatorLength + 1; j ++){
 				if((pagination[j].className === pagination[i].className) && (j !== i)){
 					pagination[j].className = "";
 				}
 			}
 			
-			for(let i = table.rows.length - 1; i > 0; i --){
+			for(let i = tRowsLength - 1; i > 0; i --){
 				tr[i].style.display = "none";
 			}
 			
-			if(i === 0){
-				for(let i = 1; i < 11; i ++){
-					tr[i].style.display = "";
-				}
-			}else if(i === paginatorLength+1){
-				let sum = ((paginatorLength-1)*10);
-				for(let i = sum+1; i < sum+11; i ++){
-					tr[i].style.display = "";
-				}
-			}else if(i > 0 || i < paginatorLength+1){
-				let sum = ((i-1)*10);
-				for(let i = sum+1; i < sum+11; i ++){
+			if(i < paginatorLength + 1){
+				let sum = ((i - 1) * 10);
+				for(let i = sum + 1; i < sum + 11; i ++){
 					tr[i].style.display = "";
 				}
 			}
 		});
 	}
+	
+	pagination[12].addEventListener('click' , function(){
+		
+		for(let i = paginatorLength - 1; i > 0; i --){
+			
+			if(pagination[i].className === "active"){
+				pagination[i + 1].className = "active";
+				pagination[i].className = "";
+				
+				for(let i = tRowsLength - 1; i > 0; i --){
+					tr[i].style.display = "none";
+				}
+				
+				if((i + 1) < paginatorLength + 1){
+					let sum = (((i + 1) - 1) * 10);
+					for(let i = sum + 1; i < sum + 11; i ++){
+						tr[i].style.display = "";
+					}
+				}
+			}
+		}
+		
+	});
 });
