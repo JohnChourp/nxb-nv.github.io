7332d7fc65063ee6bc42d2716288fcfc2f1d9407
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-11-01 18:42:08 +0200

    changed some photos link

--- Characters/JS/characterTable.js
+++ Characters/JS/characterTable.js
@@ -1,19 +1,19 @@
 function characterTable(){
 	switch(document.getElementById("charType").innerHTML){
 		case "Assist":
-			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/BR61eSPgEHta1Bqu6XfV8hNtNq7fYP6VQdHCC6CBt-XodGNyxsfBOCgInPSLGmNA8i_G8OX6DCLOr1EzBYmJhJMFpCxFJZZnesU38zSmWBzYjRqmt1rMxA62BnmjbbU_Z3Zrt7w28g=w1920-h1080";
+			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/pw/AM-JKLXq-L9q0FAEEzlbEGlN8I9ZtHidN6Qdz5QtGnUEm3c9NNSO7iObEpFe6mCUPjQtpqCvoPLI59BR9WiaKKkaWf-DzAToVe8c3AAU8hx5wvA_NUQGPRlCCTaiMhinCo4oXhOWbfxR3WBryqDA174xh2V6=w73-h78-no?authuser=0";
 			break;
 		case "Attack":
-			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/YpVsn86SLRc9UoGMy0_EmJhAvh4Rn9qm9ImhI8YbDbyXAmc2osaEudSgCS0pcuDFPQbVkAuLCFfEaNHoL12VgwBl4HnBmKqm-ZdVXKTHjgozcK7D6ziJuPAmUbgdYmTPApa8U1beCQ=w1920-h1080";
+			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/pw/AM-JKLWSXZaWknPkJDShyEyaGbzSodlnbLpP5sG7B3jgzjlhbgIPNnOMrP_rsgYdTUyY82tmyqc3IhhsHeLVI6GJlFXOQjFibl1emU8yCK3_CgjbVqqnLmP9ksvKKPecCb1enrwaPZG_T-lZICBoEmLrkART=w71-h79-no?authuser=0";
 			break;
 		case "Defense":
-			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/WqY-r_pyKeWGfkFmm3cwHTiC-8YLrx1732uuyxpHjyip4RNJk_PIbouXuNESfEYbWelb9Xpiy-LHh6zvb6vMYc1rD42a22X0diJYqXzBkgB1vvZDrZn-uwLdaJCS2iZx5PCdeU8nRg=w1920-h1080";
+			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/pw/AM-JKLUaOd9pg98atipKakrH21ZwecEx3nKXaEyVm9a9eOUf4-qi1rrWK6BeZp2qO-os7qcyZPisvlUtP8rxtH7uOsPQUGpxiROroTTguXkaP35_cRDs1-_F-Ye14g8gvM8stGwF-jiPu_ZUHmnyvpEDpc6b=w72-h77-no?authuser=0";
 			break;
 		case "Skill":
-			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/FWU-cAZZI-JGONyeUaJWiRwrgoX7GxJBtZEflHfMeeirsdUBzS0NcS2Ww5abDojJgg93qKDgOMDlVhZasHH8HVg05Ubh3w5urikAt2KV1iKRkO32HiIIgupxNZyQeBzKtXCKWX9naQ=w1920-h1080";
+			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/pw/AM-JKLVlQi7WaewND4eyQRn11yM3eURB5ToAxVkWbB-vkDzJIgcBk1pXEPY2gHQ42l2pAiVO-MNcHaD5ku0wD5icdPIzC0jUdovimi16sMXk17DGi4yIv4fS_Gi8CVL2Rep22mYjkk4nUgr6QCcMXjFoHtbZ=w72-h77-no?authuser=0";
 			break;
 		case "Utility":
-			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/xRkssCYG3PU6GsqeKaY_BQiHABwcMsYd_AxNwc6UDWucszTdFCeoJKxCKeJ6exT1RJ-UPc_JRDpKlGWW7G4iE0BBgH8fKnRqH3AUa2iLvoFV-7f7B-rWXmGNYQa5BbfV9M0AI430qQ=w1920-h1080";
+			document.getElementById("charTypeImage").src = "https://lh3.googleusercontent.com/pw/AM-JKLVmbOQGFz6KDKDr-ar9il-9XKVtcxmGiija84zQzvLFUnOSzgVZX-UzCwQdzAvFK03K-faTrArKuYDBbn3Ll2ZJn2FbkWQKiOpWvtVfgSU_DR3QYY26WX5CLgfYhyJtUBzbtxRYIixk7cvtMpCWHt34=w70-h78-no?authuser=0";
 			break;
 	}
 }
