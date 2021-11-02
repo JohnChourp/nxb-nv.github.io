304199aa308e4b7743a43b5985b48c1722b9c1b6
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-11-02 14:10:59 +0200

    now scrollbar colors are saved in night mode

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -11,7 +11,14 @@ function darkModeSave(){
 	for(let i = 0; i < 13; i ++){
 		document.getElementById("pagination").children.item(i).style.color = localStorage.getItem("paginationTextColor");
 	}
-	root.style.setProperty('--bg-color-paginator' , localStorage.getItem("paginationBgColor"));
+	
+	root.style.setProperty('--bg-color-paginator' , localStorage.getItem("bgColorPaginator"));
+	root.style.setProperty('--border-pagination-a' , localStorage.getItem("borderPaginationA"));
+	root.style.setProperty('--bg-color-paginator-a-hover-not-active' , localStorage.getItem("bgColorPaginatorAHoverNotActive"));
+	
+	root.style.setProperty('--background-webkit-scrollbar-track' , localStorage.getItem("bgColorWebkitScrollbarTrack"));
+	root.style.setProperty('--background-webkit-scrollbar-thumb' , localStorage.getItem("bgColorWebkitScrollbarThumb"));
+	root.style.setProperty('--background-webkit-scrollbar-thumb-hover' , localStorage.getItem("bgColorWebkitScrollbarThumbHover"));
 	
 	themeSwitch.addEventListener("change" , function(e){
 		if(e.currentTarget.checked === true){
@@ -21,8 +28,15 @@ function darkModeSave(){
 			localStorage.setItem("textColor" , "white");
 			localStorage.setItem("myInputBgColor" , "#404040");
 			localStorage.setItem("myInputTextColor" , "white");
+			
 			localStorage.setItem("paginationTextColor" , "white");
-			localStorage.setItem("paginationBgColor" , "#7C7A7A");
+			localStorage.setItem("bgColorPaginator" , "#404040");
+			localStorage.setItem("borderPaginationA" , "1px solid #DDD");
+			localStorage.setItem("bgColorPaginatorAHoverNotActive" , "#7C7A7A");
+			
+			localStorage.setItem("bgColorWebkitScrollbarTrack" , "#323232");
+			localStorage.setItem("bgColorWebkitScrollbarThumb" , "#888");
+			localStorage.setItem("bgColorWebkitScrollbarThumbHover" , "#696868");
 			
 			location.reload();
 		}else{
@@ -33,7 +47,13 @@ function darkModeSave(){
 			localStorage.removeItem("myInputBgColor");
 			localStorage.removeItem("myInputTextColor");
 			localStorage.removeItem("paginationTextColor");
-			localStorage.removeItem("paginationBgColor");
+			localStorage.setItem("bgColorPaginator" , "white");
+			localStorage.setItem("borderPaginationA" , "1px solid black");
+			localStorage.setItem("bgColorPaginatorAHoverNotActive" , "#989696");
+			
+			localStorage.setItem("bgColorWebkitScrollbarTrack" , "#F0F0F0");
+			localStorage.setItem("bgColorWebkitScrollbarThumb" , "#CECECE");
+			localStorage.setItem("bgColorWebkitScrollbarThumbHover" , "#A7A7A7");
 			
 			location.reload();
 		}
