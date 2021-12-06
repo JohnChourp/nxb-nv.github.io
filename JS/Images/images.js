334b15f9bc9908158916cd314fae40060647e69b
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-12-06 13:06:28 +0200

    added the ninjutsu cards in a page

--- JS/Images/images.js
+++ JS/Images/images.js
@@ -170,3 +170,15 @@ function statTypeImages(){
 		}
 	}
 }
+
+function rtImages(){
+	let rtImageLength = document.getElementsByClassName("rtImage");
+	
+	for(let i = 0; i < rtImageLength.length; i ++){
+		switch(document.getElementsByClassName("rtImage").item(i).alt){
+			case "rt":
+				document.getElementsByClassName("rtImage").item(i).src = "https://lh3.googleusercontent.com/pw/AM-JKLUv-Fe1wcfeKi0U0IhaivmWj8e9CgKhd8lMX-7TsqwxP6MeQOuBzmLTvnGA5jLmaqQL_ETKz17pzEYuKXgqJPW3cH7vieoI_5-c1GXUuKuoTasvOOR1lysGVRDEMRyA0ppSDrUEhmpB1Dqn9qIEd16t=s51-no?authuser=0";
+				break;
+		}
+	}
+}
--- JS/searchTable.js
+++ JS/searchTable.js
@@ -97,3 +97,143 @@ function sortIndex(n){
 	}
 }
 
+function searchAllNinjutsuCards(){
+	let inputCategory = document.getElementById("myInput");
+	let inputCard = document.getElementById("myInput");
+	let inputType = document.getElementById("myInput");
+	let inputJutsu = document.getElementById("myInput");
+	let inputNature = document.getElementById("myInput");
+	let inputRankLv = document.getElementById("myInput");
+	let inputJutsuLv = document.getElementById("myInput");
+	let inputCPCost = document.getElementById("myInput");
+	let inputCRI = document.getElementById("myInput");
+	let inputPOW = document.getElementById("myInput");
+	let inputRT = document.getElementById("myInput");
+	let inputEquipBy = document.getElementById("myInput");
+	
+	let filterCategory = inputCategory.value.toUpperCase();
+	let filterCard = inputCard.value.toUpperCase();
+	let filterType = inputType.value.toUpperCase();
+	let filterJutsu = inputJutsu.value.toUpperCase();
+	let filterNature = inputNature.value.toUpperCase();
+	let filterRankLv = inputRankLv.value.toUpperCase();
+	let filterJutsuLv = inputJutsuLv.value.toUpperCase();
+	let filterCPCost = inputCPCost.value.toUpperCase();
+	let filterCRI = inputCRI.value.toUpperCase();
+	let filterPOW = inputPOW.value.toUpperCase();
+	let filterRT = inputRT.value.toUpperCase();
+	let filterEquipBy = inputEquipBy.value.toUpperCase();
+	
+	let table = document.getElementById("sortTable");
+	let tr = table.getElementsByTagName("tr");
+	
+	for(let i = 0; i < tr.length; i ++){
+		
+		let tdCategory = tr[i].getElementsByTagName("td")[1];
+		let tdCard = tr[i].getElementsByTagName("td")[3];
+		let tdType = tr[i].getElementsByTagName("td")[4];
+		let tdJutsu = tr[i].getElementsByTagName("td")[6];
+		let tdNature = tr[i].getElementsByTagName("td")[8];
+		let tdRankLv = tr[i].getElementsByTagName("td")[11];
+		let tdJutsuLv = tr[i].getElementsByTagName("td")[12];
+		let tdCPCost = tr[i].getElementsByTagName("td")[13];
+		let tdCRI = tr[i].getElementsByTagName("td")[14];
+		let tdPOW = tr[i].getElementsByTagName("td")[15];
+		let tdRT = tr[i].getElementsByTagName("td")[17];
+		let tdEquipBy = tr[i].getElementsByTagName("td")[18];
+		
+		if(tdCategory && tdCard && tdType && tdJutsu && tdNature && tdRankLv && tdJutsuLv && tdCPCost && tdCRI && tdPOW && tdRT && tdEquipBy){
+			let txtCategory = tdCategory.textContent || tdCategory.innerText;
+			let txtCard = tdCard.textContent || tdCard.innerText;
+			let txtType = tdType.textContent || tdType.innerText;
+			let txtJutsu = tdJutsu.textContent || tdJutsu.innerText;
+			let txtNature = tdNature.textContent || tdNature.innerText;
+			let txtRankLv = tdRankLv.textContent || tdRankLv.innerText;
+			let txtJutsuLv = tdJutsuLv.textContent || tdJutsuLv.innerText;
+			let txtCPCost = tdCPCost.textContent || tdCPCost.innerText;
+			let txtCRI = tdCRI.textContent || tdCRI.innerText;
+			let txtPOW = tdPOW.textContent || tdPOW.innerText;
+			let txtRT = tdRT.textContent || tdRT.innerText;
+			let txtEquipBy = tdEquipBy.textContent || tdEquipBy.innerText;
+			
+			if(txtCategory.toUpperCase().indexOf(filterCategory) > - 1 || txtCard.toUpperCase().indexOf(filterCard) > - 1 || txtType.toUpperCase().indexOf(filterType) > - 1 || txtJutsu.toUpperCase().indexOf(filterJutsu) > - 1 || txtNature.toUpperCase().indexOf(filterNature) > - 1 || txtRankLv.toUpperCase().indexOf(filterRankLv) > - 1 || txtJutsuLv.toUpperCase().indexOf(filterJutsuLv) > - 1 || txtCPCost.toUpperCase().indexOf(filterCPCost) > - 1 || txtCRI.toUpperCase().indexOf(filterCRI) > - 1 || txtPOW.toUpperCase().indexOf(filterPOW) > - 1 || txtRT.toUpperCase().indexOf(filterRT) > - 1 || txtEquipBy.toUpperCase().indexOf(filterEquipBy) > - 1){
+				tr[i].style.display = "";
+			}else{
+				tr[i].style.display = "none";
+			}
+		}
+	}
+}
+
+function sortAllNinjutsuCards(n){
+	let table , rows , switching , i , x , y , shouldSwitch , dir , switchCount = 0;
+	
+	table = document.getElementById("sortTable");
+	switching = true;
+	//Set the sorting direction to ascending:
+	dir = "asc";
+	/*Make a loop that will continue until
+	 no switching has been done:*/
+	while(switching){
+		//start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		/*Loop through all table rows (except the
+		 first, which contains table headers):*/
+		for(i = 1; i < (rows.length - 1); i ++){
+			//start by saying there should be no switching:
+			shouldSwitch = false;
+			/*Get the two elements you want to compare,
+			 one from current row and one from the next:*/
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			/*check if the two rows should switch place,
+			 based on the direction, asc or desc:*/
+			if(n === 17 || n === 14 || n === 13){
+				if(dir === "asc"){
+					if(parseInt(x.innerHTML) > parseInt(y.innerHTML)){
+						//if so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}else if(dir === "desc"){
+					if(parseInt(x.innerHTML) < parseInt(y.innerHTML)){
+						//if so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}
+			}else{
+				if(dir === "asc"){
+					if(x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()){
+						//if so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}else if(dir === "desc"){
+					if(x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()){
+						//if so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}
+			}
+		}
+		if(shouldSwitch){
+			/*If a switch has been marked, make the switch
+			 and mark that a switch has been done:*/
+			rows[i].parentNode.insertBefore(rows[i + 1] , rows[i]);
+			switching = true;
+			//Each time a switch is done, increase this count by 1:
+			switchCount ++;
+		}else{
+			/*If no switching has been done AND the direction is "asc",
+			 set the direction to "desc" and run the while loop again.*/
+			if(switchCount === 0 && dir === "asc"){
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
+}
+
