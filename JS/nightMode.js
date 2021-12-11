97ed9d5d156b527f409e3567db3e47247fbda49a
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2021-12-11 16:28:37 +0200

    renamed many vars so that it's easier to understand

--- JS/nightMode.js
+++ JS/nightMode.js
@@ -1,55 +1,54 @@
 function darkModeOptions(){
-	localStorage.setItem("bgColorBody" , "#404040");
-	localStorage.setItem("textColorBody" , "white");
+	localStorage.setItem("bg-color-body" , "#404040");
+	localStorage.setItem("text-color-body" , "white");
 	
-	localStorage.setItem("myInputBgColor" , "#404040");
-	localStorage.setItem("myInputTextColor" , "white");
+	localStorage.setItem("bg-color-myInput" , "#404040");
+	localStorage.setItem("text-color-myInput" , "white");
 	
-	localStorage.setItem("paginationTextColor" , "white");
-	localStorage.setItem("bgColorPaginator" , "#404040");
-	localStorage.setItem("borderPaginatorA" , "1px solid #DDD");
-	localStorage.setItem("bgColorPaginatorAHoverNotActive" , "#7C7A7A");
+	localStorage.setItem("text-color-paginator" , "white");
+	localStorage.setItem("bg-color-paginator" , "#404040");
+	localStorage.setItem("border-a-paginator" , "1px solid #DDD");
+	localStorage.setItem("bg-color-a-hover-not-active-paginator" , "#7C7A7A");
 	
-	localStorage.setItem("bgColorWebkitScrollbarTrack" , "#323232");
-	localStorage.setItem("bgColorWebkitScrollbarThumb" , "#888");
-	localStorage.setItem("bgColorWebkitScrollbarThumbHover" , "#696868");
+	localStorage.setItem("bg-color-webkit-scrollbar-track" , "#323232");
+	localStorage.setItem("bg-webkit-scrollbar-thumb" , "#888");
+	localStorage.setItem("bg-webkit-scrollbar-thumb-hover" , "#696868");
 }
 
 function lightModeOptions(){
-	localStorage.setItem("bgColorBody" , "white");
-	localStorage.setItem("textColorBody" , "black");
+	localStorage.setItem("bg-color-body" , "white");
+	localStorage.setItem("text-color-body" , "black");
 	
-	localStorage.removeItem("myInputBgColor");
-	localStorage.removeItem("myInputTextColor");
+	localStorage.removeItem("bg-color-myInput");
+	localStorage.removeItem("text-color-myInput");
 	
-	localStorage.setItem("paginationTextColor" , "black");
-	localStorage.setItem("bgColorPaginator" , "white");
-	localStorage.setItem("borderPaginatorA" , "1px solid black");
-	localStorage.setItem("bgColorPaginatorAHoverNotActive" , "#989696");
+	localStorage.setItem("text-color-paginator" , "black");
+	localStorage.setItem("bg-color-paginator" , "white");
+	localStorage.setItem("border-a-paginator" , "1px solid black");
+	localStorage.setItem("bg-color-a-hover-not-active-paginator" , "#989696");
 	
-	localStorage.setItem("bgColorWebkitScrollbarTrack" , "#F0F0F0");
-	localStorage.setItem("bgColorWebkitScrollbarThumb" , "#CECECE");
-	localStorage.setItem("bgColorWebkitScrollbarThumbHover" , "#A7A7A7");
+	localStorage.setItem("bg-color-webkit-scrollbar-track" , "#F0F0F0");
+	localStorage.setItem("bg-webkit-scrollbar-thumb" , "#CECECE");
+	localStorage.setItem("bg-webkit-scrollbar-thumb-hover" , "#A7A7A7");
 }
 
 function setModeValues(){
 	const root = document.querySelector(':root');
 	
-	root.style.setProperty('--bg-color-body' , localStorage.getItem("bgColorBody"));
-	root.style.setProperty('--text-color-body' , localStorage.getItem("textColorBody"));
+	root.style.setProperty('--bg-color-body' , localStorage.getItem("bg-color-body"));
+	root.style.setProperty('--text-color-body' , localStorage.getItem("text-color-body"));
 	
-	root.style.setProperty('--bg-color-myInput' , localStorage.getItem("myInputBgColor"));
-	root.style.setProperty('--text-color-myInput' , localStorage.getItem("myInputTextColor"));
+	root.style.setProperty('--bg-color-myInput' , localStorage.getItem("bg-color-myInput"));
+	root.style.setProperty('--text-color-myInput' , localStorage.getItem("text-color-myInput"));
 	
+	root.style.setProperty('--text-color-paginator' , localStorage.getItem("text-color-paginator"));
+	root.style.setProperty('--bg-color-paginator' , localStorage.getItem("bg-color-paginator"));
+	root.style.setProperty('--border-a-paginator' , localStorage.getItem("border-a-paginator"));
+	root.style.setProperty('--bg-color-a-hover-not-active-paginator' , localStorage.getItem("bg-color-paginator-a-hover-not-active"));
 	
-	root.style.setProperty('--text-color-paginator' , localStorage.getItem("paginationTextColor"));
-	root.style.setProperty('--bg-color-paginator' , localStorage.getItem("bgColorPaginator"));
-	root.style.setProperty('--border-paginator-a' , localStorage.getItem("borderPaginatorA"));
-	root.style.setProperty('--bg-color-paginator-a-hover-not-active' , localStorage.getItem("bgColorPaginatorAHoverNotActive"));
-	
-	root.style.setProperty('--background-webkit-scrollbar-track' , localStorage.getItem("bgColorWebkitScrollbarTrack"));
-	root.style.setProperty('--background-webkit-scrollbar-thumb' , localStorage.getItem("bgColorWebkitScrollbarThumb"));
-	root.style.setProperty('--background-webkit-scrollbar-thumb-hover' , localStorage.getItem("bgColorWebkitScrollbarThumbHover"));
+	root.style.setProperty('--bg-color-webkit-scrollbar-track' , localStorage.getItem("bg-color-webkit-scrollbar-track"));
+	root.style.setProperty('--bg-webkit-scrollbar-thumb' , localStorage.getItem("bg-webkit-scrollbar-thumb"));
+	root.style.setProperty('--bg-webkit-scrollbar-thumb-hover' , localStorage.getItem("bg-webkit-scrollbar-thumb-hover"));
 }
 
 function changeDarkMode(){
