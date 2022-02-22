ad0f4d377699ed5f3770b48583b97fcd822b14a7
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2022-02-22 13:15:28 +0200

    changed http to https

--- JS/topNav.js
+++ JS/topNav.js
@@ -7,12 +7,12 @@ function createTopNav() {
     document.body.insertAdjacentElement('afterbegin', topNavDiv);
 
     let charactersLink = document.createElement("a");
-    charactersLink.href = "http://" + hostName + "/nxb-nv.github.io/index.html";
+    charactersLink.href = "https://" + hostName + "/nxb-nv.github.io/index.html";
     charactersLink.innerHTML = "Characters";
     topNavDiv.appendChild(charactersLink);
 
     let ninjutsuCardsLink = document.createElement("a");
-    ninjutsuCardsLink.href = "http://" + hostName + "/nxb-nv.github.io/ninjutsu-cards.html";
+    ninjutsuCardsLink.href = "https://" + hostName + "/nxb-nv.github.io/ninjutsu-cards.html";
     ninjutsuCardsLink.innerHTML = "Ninjutsu Cards";
     topNavDiv.appendChild(ninjutsuCardsLink);
 }
